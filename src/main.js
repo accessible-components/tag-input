140e3af5ff26c981037a49aaaf531b9ec88f0199
Sergei Kriger <info@sergeikriger.com>
2020-11-24 08:17:43 +0100

    chore: Add more demos

--- src/main.js
+++ src/main.js
@@ -1,56 +1,139 @@
-const colors = document.getElementById('colors');
-const colorsTagInput = new TagInput(colors, {
+// Simple
+const simple = document.getElementById('simple');
+const simpleCount = simple.parentNode.querySelector('.count');
+
+const simpleTagInput = new TagInput(simple, {
+  label: 'Colors',
+  placeholder: 'Add tags',
+
+  onInit: (tags) => {
+    simpleCount.textContent = tags.length;
+  },
+
+  onTagAdd: (tag, tags) => {
+    simpleCount.textContent = tags.length;
+  },
+  onTagRemove: (tag, tags) => {
+    simpleCount.textContent = tags.length;
+  },
+  onTagUpdate: (oldTag, newTag, tags) => {
+    simpleCount.textContent = tags.length;
+  },
+});
+
+// With tags
+const tags = document.getElementById('tags');
+const tagsCount = tags.parentNode.querySelector('.count');
+
+const tagsTagInput = new TagInput(tags, {
+  tags: ['jazz', 'blues', 'rock'],
+  label: 'Genres',
+  placeholder: 'Add genres',
+
+  onInit: (tags) => {
+    tagsCount.textContent = tags.length;
+  },
+
+  onTagAdd: (tag, tags) => {
+    tagsCount.textContent = tags.length;
+  },
+  onTagRemove: (tag, tags) => {
+    tagsCount.textContent = tags.length;
+  },
+  onTagUpdate: (oldTag, newTag, tags) => {
+    tagsCount.textContent = tags.length;
+  },
+});
+
+// Disabled
+const disabled = document.getElementById('disabled');
+const disabledCount = disabled.parentNode.querySelector('.count');
+
+const disabledTagInput = new TagInput(disabled, {
+  tags: ['traveling', 'reading', 'cooking'],
+  label: 'Interests',
+  placeholder: 'Add interests',
+  disabled: true,
+
+  onInit: (tags) => {
+    disabledCount.textContent = tags.length;
+  },
+
+  onTagAdd: (tag, tags) => {
+    disabledCount.textContent = tags.length;
+  },
+  onTagRemove: (tag, tags) => {
+    disabledCount.textContent = tags.length;
+  },
+  onTagUpdate: (oldTag, newTag, tags) => {
+    disabledCount.textContent = tags.length;
+  },
+});
+
+// Control from outside
+const control = document.getElementById('control');
+const controlCount = control.parentNode.querySelector('.count');
+
+const controlTagInput = new TagInput(control, {
+  tags: ['public speaking', 'first impression'],
+  label: 'Video tags',
+  placeholder: 'Add interests',
+
+  onInit: (tags) => {
+    controlCount.textContent = tags.length;
+  },
+
+  onTagAdd: (tag, tags) => {
+    controlCount.textContent = tags.length;
+  },
+  onTagRemove: (tag, tags) => {
+    controlCount.textContent = tags.length;
+  },
+  onTagUpdate: (oldTag, newTag, tags) => {
+    controlCount.textContent = tags.length;
+  },
+});
+
+control.parentNode.querySelectorAll('[data-tag]').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    controlTagInput.addTag(e.target.dataset.tag);
+  });
+});
+
+// Custom styles
+const styles = document.getElementById('styles');
+const stylesCount = styles.parentNode.querySelector('.count');
+
+const stylesTagInput = new TagInput(styles, {
+  tags: ['html', 'css', 'javascript'],
+  label: 'Skills',
+  hiddenLabel: true,
+  placeholder: ' ',
+
+  onInit: (tags) => {
+    stylesCount.textContent = tags.length;
+  },
+
+  onTagAdd: (tag, tags) => {
+    stylesCount.textContent = tags.length;
+  },
+  onTagRemove: (tag, tags) => {
+    stylesCount.textContent = tags.length;
+  },
+  onTagUpdate: (oldTag, newTag, tags) => {
+    stylesCount.textContent = tags.length;
+  },
+});
+
+// Form
+const form = document.getElementById('form');
+const formsTagInput = new TagInput(form, {
   tags: ['red', 'green', 'red', 'blue'],
   label: 'Colors',
   placeholder: 'Add colors',
-
-  onTagAdd: (tag, tags) => {
-    console.log('onTagAdd', tag, tags);
-  },
-  onTagRemove: (tag, tags) => {
-    console.log('onTagRemove', tag, tags);
-  },
-  onTagUpdate: (oldTag, newTag, tags) => {
-    console.log('onTagUpdate', oldTag, newTag, tags);
-  },
 });
 
-colors.parentNode.addEventListener('submit', (e) => {
+form.parentNode.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('colors:submit', e.target['tag-input'].value);
-});
-
-const skills = document.getElementById('skills');
-const skillsTagInput = new TagInput(skills, {
-  tags: ['javascript', 'css', 'react', 'nodejs'],
-  label: 'Skills',
-  placeholder: 'Add skills',
-  disabled: true,
-
-  onTagAdd: (tag, tags) => {
-    console.log('onTagAdd', tag, tags);
-  },
-  onTagRemove: (tag, tags) => {
-    console.log('onTagRemove', tag, tags);
-  },
-  onTagUpdate: (oldTag, newTag, tags) => {
-    console.log('onTagUpdate', oldTag, newTag, tags);
-  },
-});
-
-const categories = document.getElementById('categories');
-const categoriesTagInput = new TagInput(categories, {
-  tags: ['traveling', 'cooking'],
-  label: 'Categories',
-  placeholder: 'Add categories',
-
-  onTagAdd: (tag, tags) => {
-    console.log('onTagAdd', tag, tags);
-  },
-  onTagRemove: (tag, tags) => {
-    console.log('onTagRemove', tag, tags);
-  },
-  onTagUpdate: (oldTag, newTag, tags) => {
-    console.log('onTagUpdate', oldTag, newTag, tags);
-  },
+  alert(`From submitted with tags ${e.target['tag-input'].value}`);
 });
